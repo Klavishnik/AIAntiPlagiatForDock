df3688117bd35d8d0e52c8740ef8e2d71cdc6519
Klavishnik <shtanov.klavishnik@yandex.ru>
2023-04-24 17:24:29 +0300

    up

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Check if virtualenv is installed
-if ! command -v virtualenv &> /dev/null
-then
-    echo "virtualenv not found, installing..."
-    pip install virtualenv
-fi
+#if ! command -v virtualenv &> /dev/null
+#then
+#    echo "virtualenv not found, installing..."
+#    pip install virtualenv
+#fi
 
 # Create and activate virtual environment
-virtualenv venv
-source venv/bin/activate
+#virtualenv venv
+#source venv/bin/activate
 
 # Install dependencies
-sudo apt install libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext tesseract-ocr flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig
+sudo apt install -y python3 python3-dev pip libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext tesseract-ocr flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig
 pip install -r requirements.txt
 
 echo "Installation complete. To activate the virtual environment, run 'source venv/bin/activate'"
