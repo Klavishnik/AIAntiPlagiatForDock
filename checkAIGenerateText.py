40301e9d8fe1e439d7f12c16184695923a3a3e5a
Klavishnik <shtanov.klavishnik@yandex.ru>
2023-04-26 12:08:18 -0400

    added new models, hson output

--- checkAIGenerateText.py
+++ checkAIGenerateText.py
@@ -6,15 +6,22 @@ import json
 import time
 import argparse
 import math
+import logging
+import os
+
+from openai_detector import OpenaiDetector
+
 
 from docx import Document
 from datetime import datetime
-from detector import OpenaiDetector
+from transformers import pipeline
+from revChatGPT.V3 import Chatbot
+
 
 #PUT UOR TOKEN
 #HOW?
 #https://github.com/promptslab/openai-detector
-bearer_token = 
+
 
 ##Пороги срабатывания у классификатора
 mark_detect_ai_possible = 75
@@ -23,6 +30,8 @@ mark_detect_ai_generate = 96
 #Сколько слов будет в каждом куске файла при разбиении
 num_words_split_gpt2  = 300
 num_words_split_class = 800
+num_words_roberta_base = 45
+num_words_roberta_large = 45
 
 def extract_text_from_docx(docx_path):
     document = Document(docx_path)
@@ -42,7 +51,7 @@ def save_text_to_txt(text, max_words):
     return all_words
 
 
-def send_to_api(content, retries=10, delay=1):
+def send_to_api_gpt2(content, retries=10, delay=1):
     url = 'https://openai-openai-detector.hf.space/'
     headers = {'Content-Type': 'application/json'}
     data = json.dumps({'text': content})
@@ -68,15 +77,13 @@ def send_to_api(content, retries=10, delay=1):
         if attempt < retries - 1:
             print(f"Retrying... (attempt {attempt + 2}/{retries})", file=sys.stderr)
             time.sleep(delay)
-        
-
     return None, None
 
 def process_files_gpt2(my_all_words):
     fake_probabilities = []
     not_loaded_files = 0
     for i in range(len(my_all_words)-1):
-        response, result = send_to_api(my_all_words[i])
+        response, result = send_to_api_gpt2(my_all_words[i])
         if response is not None and result is not None:
             fake_probability = round(result.get('fake_probability', 0) * 100, 1)
             print(f"File: {i}, Fake probability: {fake_probability}%")
@@ -98,54 +105,134 @@ def process_files_gpt2(my_all_words):
     return not_loaded_files
 
 
+
 def process_files_class(my_all_words):
     fake_probabilities = []
     od = OpenaiDetector(bearer_token)
+    
     for i in range(len(my_all_words)-1):
         response = od.detect(my_all_words[i])
-        probability = response["AI-Generated Probability"]
-        conclusion = response["Class"]
-        fake_probabilities.append(probability)
-        
-        with open(f"{args.output}", "a") as f:
-            f.write(f"Pprobability: {probability} \n ")
-            f.write(f"Class: {conclusion} \n ")
-
-        print(f"Pprobability: {probability}")
-        print(f"Class: {conclusion}")
+        if response is not None:
+            probability = response["AI-Generated Probability"]
+            conclusion = response["Class"]
+            fake_probabilities.append(probability)
+   
+            print(f"Pprobability: {probability}")
+            print(f"Class: {conclusion}")
 
     if fake_probabilities:
             avg_fake_probability = round(sum(fake_probabilities) / len(fake_probabilities), 1)
+            avg_conclusion = od.get_class_label(avg_fake_probability)
+
+            response_dict = {
+            "OpenAI Average Probability":  avg_fake_probability,
+            "OpenAI Class": avg_conclusion
+            }
+
             print(f"\n ---> Average fake probability: {avg_fake_probability}%")
-            with open(f"{args.output}", "a") as f:
-                f.write(f"\n ---> Average fake probability: {avg_fake_probability}%")
-            
+            print(f"---> Average fake class: {avg_conclusion}")
+                       
             if (avg_fake_probability > mark_detect_ai_generate):
                 print(f"\n ---> AI GENERATE!!!!")
-                with open(f"{args.output}", "a") as f:
-                    f.write(f"\n ---> AI GENERATE!")
-
+                
             elif (avg_fake_probability > mark_detect_ai_possible):
                 print(f"\n ---> AI generation possible!")
-                with open(f"{args.output}", "a") as f:
-                    f.write(f"\n ---> AI generation possible!")
+
+    return response_dict
+
+def classify_text_roberta_base(text):
+    results = detector_base(text)
+    return results
+
+def classify_text_roberta_large(text):
+    results = detector_large(text)
+    return results
+
+def process_files_roberta_base(my_all_words):   
+    num_reals = 0 
+    num_fakes = 0
+    fake_probabilities = []  
+    real_probabilities = []
+    for i in range(len(my_all_words)-1):
+        response = classify_text_roberta_base(my_all_words[i])
+        if response is not None:
+            for item in response:
+                if item['label'] == 'Fake':
+                    fake_probabilities.append(item['score'])
+                    print(f"[{i}] Fake: {item['score']}")
+                    num_fakes = num_fakes + 1
+                if item['label'] == 'Real':
+                    real_probabilities.append(item['score'])
+                    print(f"[{i}] Real: {item['score']}")
+                    num_reals = num_reals + 1 
+                    
+    if fake_probabilities:
+        avg_fake_probabilities = sum(fake_probabilities) / len(fake_probabilities)
+
+    if real_probabilities:
+        avg_real_probabilities = sum(real_probabilities) / len(real_probabilities)
+
+    print(f"\n ---> Average fake probability: {avg_fake_probabilities}%")
+    print(f"---> Average real probability: {avg_real_probabilities}")
+    print(f"\n ---> Num fakes: {num_fakes}")
+    print(f"---> Num reals: {num_reals}")
+
+    result_dict = {
+        "Roberta Base avg FAKE prob": avg_fake_probabilities,
+        "Roberta Base avg REAL prob": avg_real_probabilities,
+        "Roberta Base Num REALS": num_reals,
+        "Roberta Base Num FAKES": num_fakes
+    }
+    return result_dict
+
+
+def process_files_roberta_large(my_all_words):   
+    num_reals = 0 
+    num_fakes = 0
+    fake_probabilities = []  
+    real_probabilities = []
+    for i in range(len(my_all_words)-1):
+        response = classify_text_roberta_large(my_all_words[i])
+        if response is not None:
+            for item in response:
+                if item['label'] == 'LABEL_1':
+                    fake_probabilities.append(item['score'])
+                    print(f"[{i}] Fake: {item['score']}")
+                    num_fakes = num_fakes + 1
+                if item['label'] == 'LABEL_0':
+                    real_probabilities.append(item['score'])
+                    print(f"[{i}] Real: {item['score']}")
+                    num_reals = num_reals + 1 
+                    
+    if fake_probabilities:
+        avg_fake_probabilities = sum(fake_probabilities) / len(fake_probabilities)
+
+    if real_probabilities:
+        avg_real_probabilities = sum(real_probabilities) / len(real_probabilities)
+
+    print(f"\n ---> Average fake probability: {avg_fake_probabilities}%")
+    print(f"---> Average real probability: {avg_real_probabilities}")
+    print(f"\n ---> Num fakes: {num_fakes}")
+    print(f"---> Num reals: {num_reals}")
+
+    result_dict = {
+        "Roberta Large avg FAKE prob": avg_fake_probabilities,
+        "Roberta Large avg REAL prob": avg_real_probabilities,
+        "Roberta Large Num REALS": num_reals,
+        "Roberta Large Num FAKES": num_fakes
+    }
+    return result_dict
 
 def calculate_entropy(text):
     if not text:
         return 0
-
-    # Подсчитайте количество символов в тексте
     char_counts = {}
     for char in text:
         if char in char_counts:
             char_counts[char] += 1
         else:
             char_counts[char] = 1
-
-    # Вычислите вероятности появления каждого символа
     char_probs = [count / len(text) for count in char_counts.values()]
-
-    # Используйте формулу Шеннона для расчета энтропии
     entropy = -sum(p * math.log2(p) for p in char_probs)
 
     return entropy
@@ -166,56 +253,83 @@ def main(input_path):
 
 
 if __name__ == '__main__':
-  
+    
     parser = argparse.ArgumentParser(description="Chec AI generated text")
     parser.add_argument("-gpt2", action="store_true", help="Using GPT-2")
     parser.add_argument("-classificator", action="store_true", help="Using classifiacator")
     parser.add_argument("-entropy", action="store_true", help="Calculate entropy of text")
-    parser.add_argument("-o", "--output", required=True, type=str, help="output file")
+    parser.add_argument("-roberta_base", action="store_true", help="Using roberta base model")
+    parser.add_argument("-roberta_large", action="store_true", help="Using roberta large model")
+    parser.add_argument("-o", "--output", required=False, type=str, help="output file")
     parser.add_argument("target_dir", type=str, help="target dir with .doc(x) files")
    
     args = parser.parse_args()
 
+    if not args.entropy and not args.gpt2 and not args.classificator and not args.roberta_base and not args.roberta_large:
+        print("Check args not set!")
+        sys.exit()
+            
     files = [f for f in os.listdir(args.target_dir) if os.path.isfile(os.path.join(args.target_dir, f))]
     
     for file in files:
 
-        if not args.entropy and not args.gpt2 and not args.classificator:
-            print("System check AI not set!")
-            sys.exit()
-
+        all_words = main(args.target_dir + "/" + file)
+        json_data = {"Name": file}
+                
         print(f"   Name: {file} ")
         print(f"----------------------------------------------------------------------------")
-        with open(f"{args.output}", "a") as f:
-            f.write(f"   Name: {file} \n")
-            f.write(f"---------------------------------------------------------------------------- \n ")
-                
-        if args.entropy:
-            all_words = main(args.target_dir + "/" + file)
-            print(f"----- Entropy ----- ")
-            entropy = calculate_entropy(all_words)
-            print(f"Entropy: {entropy}")
-            with open(f"{args.output}", "a") as f:
-                f.write(f"----- ChatGPT 2.0 ----- \n ")
+
+        if args.roberta_base: 
+            detector_base = pipeline(
+                "text-classification",
+                model="roberta-base-openai-detector",
+                tokenizer="roberta-base-openai-detector"
+            )   
+            print(f"----- Roberta Base ----- ")
+            splitted_words_roberta_base = save_text_to_txt(all_words, num_words_roberta_base)
+            data_class = process_files_roberta_base(splitted_words_roberta_base)
+            json_data.update(data_class)  
+
+
+        if args.roberta_large: 
+            detector_large = pipeline(
+                "text-classification",
+                model="roberta-large-openai-detector",
+                tokenizer="roberta-large-openai-detector"
+            )   
+            print(f"----- Roberta Large ----- ")
+            splitted_words_roberta_large = save_text_to_txt(all_words, num_words_roberta_large)
+            data_class = process_files_roberta_large(splitted_words_roberta_large)
+            json_data.update(data_class)  
+
 
         if args.gpt2:
             splitted_words_gpt2 = save_text_to_txt(all_words, num_words_split_gpt2)
             print(f"----- ChatGPT 2.0 ----- ")
-            with open(f"{args.output}", "a") as f:
-                f.write(f"----- ChatGPT 2.0 ----- \n ")
             process_files_gpt2(splitted_words_gpt2)
+            #json_data.update(data_class)
 
         if args.classificator:
             splitted_words_class = save_text_to_txt(all_words, num_words_split_class)
             print(f"----- AI Text Classifier ----- ")
-            with open(f"{args.output}", "a") as f:
-                f.write(f"----- AI Text Classifier ----- \n ")      
-            process_files_class(splitted_words_class)
-
+            data_class = process_files_class(splitted_words_class)
+            json_data.update(data_class)
         
+        if args.entropy:
+            print(f"----- Entropy ----- ")
+            entropy = calculate_entropy(all_words)
+            print(f"Entropy: {entropy}")
+            json_data["Entropy"] = entropy
+            json_data.update(data_class)
+
+        if args.output: 
+            capitals_json = json.dumps(json_data)
+            with open(args.output, "a") as my_file:
+                my_file.write(capitals_json)
+                my_file.write("\n")
+            
         print("\n\n")
-        with open(f"{args.output}", "a") as f:
-            f.write("\n\n\n")
+        
         
         
         
