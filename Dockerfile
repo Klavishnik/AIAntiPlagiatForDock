894f1aebb6eee782b3f0e4cc7f355c4ae4109d0d
Klavishnik <shtanov.klavishnik@yandex.ru>
2023-06-13 10:11:48 -0400

    update

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM ubuntu:latest
 WORKDIR /home/
-RUN apt update && apt-get install -y git vim
 RUN git clone https://github.com/Klavishnik/AIAntiPlagiatForDock
 WORKDIR AIAntiPlagiatForDock
 RUN ./install.sh
--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 # run with sudo
 # Check if virtualenv is installed
 apt update
-apt install -y python3 python3-dev pip libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext tesseract-ocr flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig libpulse-dev
-               
+apt install -y python3 python3-dev pip libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext tesseract-ocr flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig libpulse-dev git vim curl
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt-get install git-lfs
+        
 
 if ! command -v virtualenv &> /dev/null
 then
@@ -21,7 +23,7 @@ pip install -r requirements.txt
 
 echo "Installation complete. To activate the virtual environment, run 'source venv/bin/activate'"
 
-
+git lfs install
 git clone  https://huggingface.co/roberta-large-openai-detector roberta-large-openai-detector
 git clone  https://huggingface.co/roberta-base-openai-detector roberta-base-openai-detector
 
