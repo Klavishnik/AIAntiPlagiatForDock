d80803cf80da7b2d4529ecd7f6f7a4555e990f41
Klavishnik <shtanov.klavishnik@yandex.ru>
2023-06-13 09:13:11 -0400

    update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
-FROM ubuntu:lastest
+FROM ubuntu:latest
 WORKDIR /home/
 RUN apt update && apt-get install -y git vim
-RUN git clone https://github.com/Klavishnik/AIAntiPlagiatForDock && cd AIAntiPlagiatForDock
+RUN git clone https://github.com/Klavishnik/AIAntiPlagiatForDock
+WORKDIR AIAntiPlagiatForDock
 RUN ./install.sh
--- install.sh
+++ install.sh
@@ -22,25 +22,7 @@ pip install -r requirements.txt
 echo "Installation complete. To activate the virtual environment, run 'source venv/bin/activate'"
 
 
-mkdir tmp roberta-base-openai-detector roberta-large-openai-detector
-cd tmp
-git clone  https://huggingface.co/roberta-large-openai-detector tmp/
-git clone  https://huggingface.co/roberta-base-openai-detector tmp/
-cd ..
-
-cp tmp/roberta-base-openai-detector/config.json roberta-base-openai-detector
-cp tmp/roberta-base-openai-detector/merges.txt roberta-base-openai-detector
-cp tmp/roberta-base-openai-detector/pytorch_model.bin roberta-base-openai-detector
-cp tmp/roberta-base-openai-detector/tokenizer.json roberta-base-openai-detector
-cp tmp/roberta-base-openai-detector/vocab.json roberta-base-openai-detector
-
-
-cp tmp/roberta-large-openai-detector/config.json roberta-large-openai-detector
-cp tmp/roberta-large-openai-detector/merges.txt roberta-large-openai-detector
-cp tmp/roberta-large-openai-detector/pytorch_model.bin roberta-large-openai-detector
-cp tmp/roberta-large-openai-detector/tokenizer.json roberta-large-openai-detector
-cp tmp/roberta-large-openai-detector/vocab.json roberta-large-openai-detector
-
-rm -rf tmp
+git clone  https://huggingface.co/roberta-large-openai-detector roberta-large-openai-detector
+git clone  https://huggingface.co/roberta-base-openai-detector roberta-base-openai-detector
 
 #source venv/bin/activate
